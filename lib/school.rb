fb4262cfd199967ae2408aa08ec55ef6ad53b6ac
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-09-11 23:32:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require "pry"
+
+class School 
+  attr_accessor :name, :roster
+
+def initialize(name)
+  @name = name
+  @roster = {} 
+end
+
+def add_student(name, grade)
+  if roster[grade]
+    roster[grade] << name
+  else
+   roster[grade] = [name] 
+  end
+end
+
+def grade(grade)
+  roster[grade]
+end
+
+def sort
+  roster.sort_by { |key, val| val }
+end
+  
+end
